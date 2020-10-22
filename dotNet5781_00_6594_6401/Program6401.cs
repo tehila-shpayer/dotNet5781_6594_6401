f6381c759304c2592960140125aa632200be209f
Sarah Malka <sarahmal@g.jct.ac.il>
2020-10-22 20:18:49 +0300

    welcome6401

---
+++ dotNet5781_00_6594_6401/Program6401.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6594_6401
+{
+    partial class Program
+    {
+        static partial void welcome6401()
+        {
+            Console.WriteLine("Tehila! I like to work with you:)");
+        }
+    }
+}
