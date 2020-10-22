0997e14e8a48b07691a3096c8980bd233cdaec6c
tehilaSH <shpayer@g.jct.ac.il>
2020-10-22 19:10:22 +0300

    initial code - welcome6594

---
+++ dotNet5781_00_6594_6401/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6401_6594
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcom6401();
+            welcome6594();
+            Console.ReadKey();
+        }
+
+        static partial void welcome6594();
+
+        private static void welcom6401()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to my first console application", name);
+        }
+    }
+}
