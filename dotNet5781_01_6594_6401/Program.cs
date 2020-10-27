2dcd52b6a1e3d22bae9e51d2916d15ef4eb7e040
tehilaSH <shpayer@g.jct.ac.il>
2020-10-27 20:32:36 +0200

    פתחתי את הפרוייקט של תרגיל 1 במאגר

---
+++ dotNet5781_01_6594_6401/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_6594_6401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
