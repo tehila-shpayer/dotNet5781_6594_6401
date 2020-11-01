ec7669bfc3d3ab35e38bc311b201b308b27b6645
Sarah Malka <sarahmal@g.jct.ac.il>
2020-11-01 19:54:44 +0200

    g

--- dotNet5781_02_6594_6401/BusLine.cs
+++ dotNet5781_02_6594_6401/BusLine.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_02_6594_6401
 {
+    enum Areas { Jerusalem, Center, North, South, }
     class BusLine
     {
+        public List<BusLineStation> BusLineStations { get; private set; }
+        public int LineNumber { get; private set; }
+        public BusStation FirstStation { get; private set; }
+        public BusStation LastStation { get; private set; }
+
+        public Areas area { get; private set; }
     }
 }
