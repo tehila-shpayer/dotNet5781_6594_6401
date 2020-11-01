d9245584d3484c4e73607c3ef3f2643173fc89c9
tehilaSH <shpayer@g.jct.ac.il>
2020-11-01 19:47:14 +0200

    small change

--- dotNet5781_02_6594_6401/Program.cs
+++ dotNet5781_02_6594_6401/Program.cs
@@ -12,6 +12,7 @@ namespace dotNet5781_02_6594_6401
         {
             BusStation bs = new BusStation(158745, 31.234567, 34.56789, "רח' פלוני אלמוני 12, תל חורף");
             Console.WriteLine(bs);
+            BusLineStation bss = new BusLineStation(158745, 31.234567, 34.56789,-18,54, "רח' פלוני אלמוני 12, תל חורף");
         }
     }
 }
