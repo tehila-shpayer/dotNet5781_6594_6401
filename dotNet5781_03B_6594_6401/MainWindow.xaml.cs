ecae559f0f0c83b28a55f9a7b090d87e69c72e74
Sarah Malka <sarahmal@g.jct.ac.il>
2020-11-29 23:58:49 +0200

    י

--- dotNet5781_03B_6594_6401/MainWindow.xaml.cs
+++ dotNet5781_03B_6594_6401/MainWindow.xaml.cs
@@ -22,25 +22,53 @@ namespace dotNet5781_03B_6594_6401
     /// </summary>
     public partial class MainWindow : Window
     {
-        int i = 0;
-        public MainWindow()
+        public void RandomInitializationBus()
         {
             Random rand = new Random(DateTime.Now.Millisecond);
-            Bus bus=new Bus();
+            Bus bus = new Bus();
             for (int i = 0; i < 10; i++)
             {
-                string s = rand.Next(1000000, 99999999).ToString();
-                if (i % 4 == 0)
-                    bus = new Bus(new DateTime(i + 2000, i+1, (i * i + 1) % 29 + 1), s, i * 100, 19000, 19000);
-                else if (i % 5 == 0)
-                    bus = new Bus(new DateTime(i + 2000, (i * i + 1) % 12, (i * i + 1) % 29 + 1), s, i * 10);
+                String s;
+                int year;
+                if (i % 2 == 0)
+                {
+                    s = rand.Next(1000000, 9999999).ToString();
+                    year = rand.Next(1895, 2018);
+                }
                 else
-                    bus = new Bus(new DateTime(i + 2000, (i * i + 1) % 12, (i * i) % 29 + 1), s);
+                {
+                    s = rand.Next(10000000, 99999999).ToString();
+                    year = rand.Next(2018, DateTime.Now.Year + 1);
+                }
+                int KM = rand.Next(0, 10000);
+                int bt = rand.Next(0, Max(rand.Next(0, 20000), KM));
+
+
+                //if (i % 4 == 0)
+                //    bus = new Bus(new DateTime(i + 2000, i + 1, (i * i + 1) % 29 + 1), s, i * 100, 19000, 19000);
+                //else if (i % 5 == 0)
+                //    bus = new Bus(new DateTime(i + 2000, (i * i + 1) % 12, (i * i + 1) % 29 + 1), s, i * 10);
+                //else
+                //    bus = new Bus(new DateTime(i + 2000, (i * i + 1) % 12, (i * i) % 29 + 1), s);
+
+                bus = new Bus(new DateTime(year, rand.Next(1, 13), rand.Next(1, 32)), s, rand.Next(0, 1201), KM, bt);
                 BusCollection.Add(bus);
             }
+
+        }
+        public int Max(int a, int b)
+        {
+            if (a < b)
+                return a;
+            return b;
+        }
+        public MainWindow()
+        {
+            RandomInitializationBus();
+            
             InitializeComponent();
         }
-
+        int i = 0;
         private void output_KeyDown(object sender, KeyEventArgs e)
         {
             output.Text = BusCollection.buses.ElementAt(i).ToString();
