23dd846a8faffa8270e00e177a76cd2f0b0d5374
tehila1742 <tehila1742@gmail.com>
2020-12-10 18:20:10 +0200

    2

--- dotNet5781_03B_6594_6401/RideWindow.xaml.cs
+++ dotNet5781_03B_6594_6401/RideWindow.xaml.cs
@@ -21,43 +21,37 @@ namespace dotNet5781_03B_6594_6401
     /// </summary>
     public partial class RideWindow : Window
     {
-        BackgroundWorker rider;
         public RideWindow(int index)
         {
             InitializeComponent();
-            rider = new BackgroundWorker();
-            rider.DoWork += Rider_DoWork;
-            rider.ProgressChanged += Rider_ProgressChanged;
-            rider.RunWorkerCompleted += Rider_RunWorkerCompleted;
-            rider.WorkerReportsProgress = true;
             DataContext = index;
         }
-        private void Rider_DoWork(object sender, DoWorkEventArgs e)
-        {
-            rider.ReportProgress(0);
-            int KM = (int)e.Argument;
-            Random rnd = new Random();
-            double time =(double) KM / rnd.Next(30, 60);
-            int timeToSleep = (int)(time * 6000);
-            Thread.Sleep(timeToSleep);
-            rider.ReportProgress(KM);
-        }
-        private void Rider_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            int index = (int)DataContext;
-            int KM = e.ProgressPercentage;
-            if (KM == 0)
-            {
-                BusCollection.windowBuses[index].BusStatus = Status.Ride;
-            }
-            else
-                BusCollection.windowBuses[index].Ride(KM);
-        }
-        private void Rider_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            String s = "Bus " + BusCollection.windowBuses[(int)DataContext].GetLicenseNumberFormat() + " has successfully finished the ride!";
-            MessageBox.Show(s, "Ride Massage", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
+        //private void Rider_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    rider.ReportProgress(0);
+        //    int KM = (int)e.Argument;
+        //    Random rnd = new Random();
+        //    double time =(double) KM / rnd.Next(30, 60);
+        //    int timeToSleep = (int)(time * 6000);
+        //    Thread.Sleep(timeToSleep);
+        //    rider.ReportProgress(KM);
+        //}
+        //private void Rider_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        //{
+        //    int index = (int)DataContext;
+        //    int KM = e.ProgressPercentage;
+        //    if (KM == 0)
+        //    {
+        //        BusCollection.windowBuses[index].BusStatus = Status.Ride;
+        //    }
+        //    else
+        //        BusCollection.windowBuses[index].Ride(KM);
+        //}
+        //private void Rider_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        //{
+        //    String s = "Bus " + BusCollection.windowBuses[(int)DataContext].GetLicenseNumberFormat() + " has successfully finished the ride!";
+        //    MessageBox.Show(s, "Ride Massage", MessageBoxButton.OK, MessageBoxImage.Information);
+        //}
 
         private void KM_PreviewKeyDown(object sender, KeyEventArgs e)
         {
