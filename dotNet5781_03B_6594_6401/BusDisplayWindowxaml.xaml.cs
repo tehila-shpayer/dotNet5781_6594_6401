438e8360955e0c5525c6504d6c20bc65a233bbef
tehila1742 <tehila1742@gmail.com>
2020-12-10 22:33:08 +0200

    1

---
+++ dotNet5781_03B_6594_6401/BusDisplayWindowxaml.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dotNet5781_03B_6594_6401
+{
+    /// <summary>
+    /// Interaction logic for BusDisplayWindowxaml.xaml
+    /// </summary>
+    public partial class BusDisplayWindowxaml : Window
+    {
+        public BusDisplayWindowxaml()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busViewSource.Source = [generic data source]
+        }
+    }
+}
