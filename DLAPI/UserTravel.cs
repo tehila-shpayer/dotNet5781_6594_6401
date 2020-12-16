3f4235f709e001f85e7d49b719c3b591171f3d85
tehila1742 <tehila1742@gmail.com>
2020-12-16 18:39:29 +0200

    end user classes

--- DLAPI/UserTravel.cs
+++ DLAPI/UserTravel.cs
@@ -8,6 +8,16 @@ namespace DO
 {
     class UserTravel
     {
-        public static int 
+        public string UserName { get; set; }
+        public int BusLineKey { get; set; }
+        public int UpStationKey { get; set; } 
+        public int DownStationKey { get; set; }
+        public string UpTime { get; set; }
+        public string DownTime { get; set; }
+        public override string ToString()
+        {
+            return this.ToStringProperty<UserTravel>();
+        }
+
     }
 }
