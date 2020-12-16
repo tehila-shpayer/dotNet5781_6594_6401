0f1f5c8b333b71686917f2496fdfc52d39cb0453
Sarah Malka <sarahmal@g.jct.ac.il>
2020-12-16 18:54:00 +0200

    f

---
+++ DLAPI/LineSchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class LineSchedule
+    {
+        public int BusLineNumer { get; set; }
+        public int Frequency { get; set; }
+        public override string ToString()
+        {
+            return this.ToStringProperty();
+        }
+    }
+}
--- DLAPI/Station.cs
+++ DLAPI/Station.cs
@@ -8,7 +8,6 @@ namespace DO
 {
     public class Station
     {
-        public static int STATION_KEY = 0;
         public int Key { get; private set; }
         public double Latitude { get; private set; }
         public double Longitude { get; private set; }
