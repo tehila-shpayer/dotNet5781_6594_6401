9ffb2d395c173a17af0cca330e4841c9ac041d04
Sarah Malka <sarahmal@g.jct.ac.il>
2020-12-16 16:50:44 +0200

    עוד מחלקות

--- DLAPI/BusLine.cs
+++ DLAPI/BusLine.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    public class BusInTravel
+    public class BusLine
     {
-
     }
 }
--- DLAPI/Station.cs
+++ DLAPI/Station.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DLAPI
+namespace DO
 {
-    class Station
+    public class Station
     {
     }
 }
