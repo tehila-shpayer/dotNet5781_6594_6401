35ef25c371aaf55d39226ae262437d1911632a06
tehila1742 <tehila1742@gmail.com>
2020-12-31 19:58:12 +0200

    1

--- DLAPI/DLConfig.cs
+++ DLAPI/DLConfig.cs
@@ -1,8 +1,9 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace DLAPI
 {
