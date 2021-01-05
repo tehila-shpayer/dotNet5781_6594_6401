bff79ba3ecc9c1eb5a3a00b43db0d15b75051f48
Sarah Malka <sarahmal@g.jct.ac.il>
2021-01-05 22:36:48 +0200

    fr

---
+++ PL/PO/BusLineStation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class BusLineStation
+    {
+        public int BusLineKey { get; set; }
+        public int StationKey { get; set; }
+        public int Position { get; set; }
+        public double DistanceFromLastStationMeters { get; set; }
+        public int TravelTimeFromLastStationMinutes { get; set; }
+
+        public bool _isActive = true;
+        public bool IsActive { get; set; }
+
+    }
+}
--- PL/PO/Station.cs
+++ PL/PO/Station.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 
-namespace BO
+namespace PL
 {
     public class Station : INotifyPropertyChanged
     {
