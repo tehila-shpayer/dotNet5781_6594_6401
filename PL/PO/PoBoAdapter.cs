a468f6f43bdf36a2a782b19ec878f5814c2c1698
tehila1742 <tehila1742@gmail.com>
2021-01-06 14:57:23 +0200

    1

---
+++ PL/PO/PoBoAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+using BLAPI;
+
+namespace PL
+{
+    public static class PoBoAdapter
+    {
+        static public  PL.BusLine BusLineDoBoAdapter(BO.BusLine BusLineBO)
+        {
+           PL.BusLine BusLinePO = new PL.BusLine();
+           BusLineBO.Clone(BusLinePO);
+            return BusLinePO;
+        }
+    }
+}
