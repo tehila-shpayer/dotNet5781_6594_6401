681b0b4b51b9e03de8881977eefca304ed4c4e98
tehila1742 <tehila1742@gmail.com>
2021-01-10 16:41:10 +0200

    1

---
+++ PL/MapWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MapWindow.xaml
+    /// </summary>
+    public partial class MapWindow : Window
+    {
+        public MapWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+        }
+    }
+}
