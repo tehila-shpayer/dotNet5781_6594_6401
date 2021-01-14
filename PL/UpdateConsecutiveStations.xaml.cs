76e378b1c4617fea483f9d731694eafcde0932d5
tehila1742 <tehila1742@gmail.com>
2021-01-14 12:43:28 +0200

    1

--- PL/UpdateConsecutiveStations.xaml.cs
+++ PL/UpdateConsecutiveStations.xaml.cs
@@ -19,15 +19,28 @@ namespace PL
     /// </summary>
     public partial class UpdateConsecutiveStations : Window
     {
+        public BO.BusLineStation blsBO = new BO.BusLineStation();
         public UpdateConsecutiveStations(BusLineStation bls)
         {
             InitializeComponent();
+            bls.Clone(blsBO);
+            //blsBO.BusLineKey = bls.BusLineKey;
+            //blsBO.StationKey = bls.StationKey;
+            //blsBO.Position = bls.Position;
+            //blsBO.TravelTimeFromLastStationMinutes = bls.TravelTimeFromLastStationMinutes;
+            //blsBO.DistanceFromLastStationMeters = bls.DistanceFromLastStationMeters;
             BO.Station station1 = App.bl.GetPreviouseStation(bls.BusLineKey, bls.Position);
             BO.Station station2 = App.bl.GetStation(bls.StationKey);
-            tbFirstPosition.DataContext = station1;
-            tbSecondPosition.DataContext = station2;
+            tbTitle.DataContext = bls;
             spFirstStation.DataContext = station1;
             spSecondStation.DataContext = station2;
+            spDistanceAndTime.DataContext = bls;
+        }
+
+        private void saveChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.bl.UpdateBusLineStation(blsBO);
+            MainWindow.InitializeBusLines();
         }
     }
 }
