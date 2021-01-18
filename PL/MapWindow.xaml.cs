17769118aa764c3a940e06aa4feb47c11f18b105
tehila1742 <tehila1742@gmail.com>
2021-01-18 15:26:07 +0200

    1

--- PL/MapWindow.xaml.cs
+++ PL/MapWindow.xaml.cs
@@ -22,7 +22,6 @@ namespace PL
         public MapWindow(Station station)
         {
             InitializeComponent();
-            //wbMap.Source = $"https://www.google.com/maps/search/?api=1&query=+{station.Latitude}+,+{station.Longitude}";
             //wbMaps.Source = $"tps://www.google.com/maps/search/?api=1&query=+{station.Latitude}+,+{station.Longitude}";
         }
 
