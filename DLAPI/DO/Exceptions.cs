e1de2d4d1e0a404b0ab3cd6f4e3d9b80add40b93
Sarah Malka <sarahmal@g.jct.ac.il>
2021-01-19 17:41:09 +0200

    xd

--- DLAPI/DO/Exceptions.cs
+++ DLAPI/DO/Exceptions.cs
@@ -28,4 +28,17 @@ namespace DO
             return base.ToString() + "\nArgument Not Found Exception!\n" + Message;
         }
     }
+    public class XMLFileLoadCreateException : Exception
+    {
+        public string xmlFilePath;
+        public XMLFileLoadCreateException(string xmlPath) : base() { xmlFilePath = xmlPath; }
+        public XMLFileLoadCreateException(string xmlPath, string message) :
+            base(message)
+        { xmlFilePath = xmlPath; }
+        public XMLFileLoadCreateException(string xmlPath, string message, Exception innerException) :
+            base(message, innerException)
+        { xmlFilePath = xmlPath; }
+
+        public override string ToString() => base.ToString() + $", fail to load or create xml file: {xmlFilePath}";
+    }
 }
