45e0f342bfe737ead82c841ce675bf561c3febd4
Sarah Malka <sarahmal@g.jct.ac.il>
2021-02-08 11:14:05 +0200

    סדש

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -51,7 +51,7 @@ namespace PL
             resDictEN = Application.Current.Resources.MergedDictionaries.FirstOrDefault(a => a.Source.OriginalString == @"/res/languages/AppString_EN.xaml");
             resDictHE = Application.Current.Resources.MergedDictionaries.FirstOrDefault(a => a.Source.OriginalString == @"/res/languages/AppString_HE.xaml");
             resDictFR = Application.Current.Resources.MergedDictionaries.FirstOrDefault(a => a.Source.OriginalString == @"/res/languages/AppString_FR.xaml");
-            resDictRU = Application.Current.Resources.MergedDictionaries.FirstOrDefault(a => a.Source.OriginalString == @"/res/languages/AppString_RU.xaml");
+            //resDictRU = Application.Current.Resources.MergedDictionaries.FirstOrDefault(a => a.Source.OriginalString == @"/res/languages/AppString_RU.xaml");
             Language = Application.Current.Resources.MergedDictionaries.ElementAt(Application.Current.Resources.MergedDictionaries.Count - 1);
         }
         private void upGrid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -119,6 +119,7 @@ namespace PL
         private void EnglishBtn_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Resources.MergedDictionaries.Remove(resDictHE);
+            Application.Current.Resources.MergedDictionaries.Remove(resDictFR);
             Application.Current.Resources.MergedDictionaries.Add(resDictEN);
             Language = Application.Current.Resources.MergedDictionaries.ElementAt(Application.Current.Resources.MergedDictionaries.Count - 1);
         }
@@ -128,12 +129,12 @@ namespace PL
             Application.Current.Resources.MergedDictionaries.Add(resDictEN);
             Language = Application.Current.Resources.MergedDictionaries.ElementAt(Application.Current.Resources.MergedDictionaries.Count - 1);
         }
-        private void RussianBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Resources.MergedDictionaries.Remove(resDictHE);
-            Application.Current.Resources.MergedDictionaries.Add(resDictEN);
-            Language = Application.Current.Resources.MergedDictionaries.ElementAt(Application.Current.Resources.MergedDictionaries.Count - 1);
-        }
+        //private void RussianBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Application.Current.Resources.MergedDictionaries.Remove(resDictHE);
+        //    Application.Current.Resources.MergedDictionaries.Add(resDictEN);
+        //    Language = Application.Current.Resources.MergedDictionaries.ElementAt(Application.Current.Resources.MergedDictionaries.Count - 1);
+        //}
 
         private void contactUsButton_Click(object sender, RoutedEventArgs e)
         {
